4174f8cb5533b1d48c089cc91c0c145c84095749
dat werid Koda <xboxguy2099@tuta.io>
2025-11-01 21:42:04 -0400

    Refactor playMusic button click handler

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 console.log("Dat Weird Website initialized.");
 
-// Create music element
 const music = new Audio("music/theme.mp3");
 music.loop = true;
 
-// Play music on button click
 document.getElementById("playMusic").addEventListener("click", () => {
   music.play();
-  document.getElementById("playMusic").style.display = "none"; // hide button after clicked
+  document.getElementById("playMusic").style.display = "none"; // hide button
 });
